a8a7ceed025017e3156619567fcffd7631b980e0
Ross Searle <ross-s@live.co.uk>
2026-04-20 22:04:50 +0100

    chore: remove support for Lit from Mitosis package

--- packages/ui/mitosis.config.cjs
+++ packages/ui/mitosis.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('@builder.io/mitosis').MitosisConfig} */
 module.exports = {
-  targets: ["react", "vue", "svelte", "angular", "solid", "preact", "qwik", "lit"],
+  targets: ["react", "vue", "svelte", "angular", "solid", "preact", "qwik"],
   commonOptions: {
     typescript: true,
   },
