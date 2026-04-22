cb57ef7a9cb3402da068c867932634e4c6be6a10
Ross Searle <ross-s@live.co.uk>
2026-04-22 19:11:39 +0100

    feat: add alias for Mitosis JSX runtime in Vite config
    Configure a module alias in the Vite config to map "@builder.io/mitosis/jsx-runtime" to "react/jsx-runtime". This enhances integration capabilities by aligning Mitosis with React's JSX runtime, streamlining development within the React example project.

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@builder.io/mitosis/jsx-runtime": "react/jsx-runtime",
+    },
+  },
   server: { port: 5173 },
 });
